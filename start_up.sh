5c8ed8a7637815ee549f41e8fa02cf0cbc46f090
Issue <zhushisong@skio.com>
2017-08-16 17:28:33 +0800

    db

--- start_up.sh
+++ start_up.sh
@@ -4,9 +4,11 @@ bundle exec rake assets:precompile
 
 if [ ! -z $1 ]; then
   echo '数据库不存在'1
-  RAILS_ENV=production bundle exec rake db:create
+  # RAILS_ENV=production bundle exec rake db:create
+  docker-compose run app rake db:create
 fi
 
-RAILS_ENV=production bundle exec rake db:migrate
+# RAILS_ENV=production bundle exec rake db:migrate
+  docker-compose run app rake db:migrate
 
 bundle exec puma -C config/puma.rb
