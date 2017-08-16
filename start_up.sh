9d07b6e77316a24718dcec6b18384dd607d41732
Issue <zhushisong@skio.com>
2017-08-16 17:15:35 +0800

    add db

--- start_up.sh
+++ start_up.sh
@@ -2,4 +2,11 @@
 
 bundle exec rake assets:precompile
 
+if [ ! -z $1 ]; then
+  echo '数据库不存在'1
+  docker-compose run app rake db:create
+fi
+
+docker-compose run app rake db:migrate
+
 bundle exec puma -C config/puma.rb
